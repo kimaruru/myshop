e8d79855a42c20cd8d09c1ca5a327bcc02528d51
kimaruru <kimaruru@gmail.com>
2019-08-14 14:03:31 +0800

    新增商品前後台

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -29,11 +29,11 @@ class UserController extends AdminController
         $grid->column('id', __('Id'));
         $grid->column('name', __('Name'));
         $grid->column('email', __('Email'));
-        //$grid->column('email_verified_at', __('Email verified at'));
-        //$grid->column('password', __('Password'));
+        $grid->column('email_verified_at', __('Email verified at'));
+        $grid->column('password', __('Password'));
         $grid->column('address', __('Address'));
         $grid->column('tel', __('Tel'));
-        //$grid->column('remember_token', __('Remember token'));
+        $grid->column('remember_token', __('Remember token'));
         $grid->column('created_at', __('Created at'));
         $grid->column('updated_at', __('Updated at'));
 
@@ -53,11 +53,11 @@ class UserController extends AdminController
         $show->field('id', __('Id'));
         $show->field('name', __('Name'));
         $show->field('email', __('Email'));
-        //$show->field('email_verified_at', __('Email verified at'));
-        //$show->field('password', __('Password'));
+        $show->field('email_verified_at', __('Email verified at'));
+        $show->field('password', __('Password'));
         $show->field('address', __('Address'));
         $show->field('tel', __('Tel'));
-        //$show->field('remember_token', __('Remember token'));
+        $show->field('remember_token', __('Remember token'));
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
 
@@ -75,11 +75,11 @@ class UserController extends AdminController
 
         $form->text('name', __('Name'));
         $form->email('email', __('Email'));
-        $form->datetime('email_verified_at', __('Email verified at'))->default(date('Y-m-d H:i:s'));
-        $form->password('password', __('Password'));
+        //$form->datetime('email_verified_at', __('Email verified at'))->default(date('Y-m-d H:i:s'));
+        //$form->password('password', __('Password'));
         $form->text('address', __('Address'));
         $form->text('tel', __('Tel'));
-        $form->text('remember_token', __('Remember token'));
+        //$form->text('remember_token', __('Remember token'));
 
         return $form;
     }
